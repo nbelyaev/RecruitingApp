38cd8f40002a76598cb545f489d39797314e6c5a
Nikita Belyaev <Nikita.Belyaev@us.sogeti.com>
2016-08-03 13:41:53 -0500

    First commit

---
+++ RecruiterApp/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RecruiterApp
+{
+	public static class Constants
+	{
+		// Replace strings with your mobile services and gateway URLs.
+		public static string ApplicationURL = @"https://recruiterapp.azurewebsites.net";
+	}
+}
+
