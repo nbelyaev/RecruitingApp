903f010a55ab50733d81b39447804c21a605141c
pintcris <cristian.pintado@us.sogeti.com>
2016-08-03 15:21:28 -0500

    Added nav bar functionality

---
+++ RecruiterApp/CandidatePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RecruiterApp
+{
+	public partial class CandidatePage : ContentPage
+	{
+		public CandidatePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
