f4de72a8103199404788941ac372129b6c3a9fc0
ericruelas <eric.ruelas@us.sogeti.com>
2016-08-04 13:33:40 -0500

    Edited manager file and added interview test page

---
+++ RecruiterApp/InterviewTypePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RecruiterApp
+{
+	public partial class InterviewTypePage : ContentPage
+	{
+		public InterviewTypePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
---
+++ RecruiterApp/ItemManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace RecruiterApp
+{
+	public partial class ItemManager
+	{
+		static ItemManager defaultInstance = new ItemManager();
+		MobileServiceClient client;
+		IMobileServiceTable<TodoItem> todoTable;
+
+		private ItemManager()
+		{
+			this.client = new MobileServiceClient(
+				Constants.ApplicationURL);
+			this.todoTable = client.GetTable<TodoItem>();
+		}
+
+		public static ItemManager DefaultManager
+		{
+			get
+			{
+				return defaultInstance;
+			}
+			private set
+			{
+				defaultInstance = value;
+			}
+		}
+
+		public MobileServiceClient CurrentClient
+		{
+			get { return client; }
+		}
+
+		public bool IsOfflineEnabled
+		{
+			get { return todoTable is Microsoft.WindowsAzure.MobileServices.Sync.IMobileServiceSyncTable<TodoItem>; }
+		}
+
+		public async Task<ObservableCollection<TodoItem>> GetTodoItemsAsync(bool syncItems = false)
+		{
+			try
+			{
+				IEnumerable<TodoItem> items = await todoTable
+					.Where(todoItem => !todoItem.Done)
+					.ToEnumerableAsync();
+
+				return new ObservableCollection<TodoItem>(items);
+			}
+			catch (MobileServiceInvalidOperationException msioe)
+			{
+				Debug.WriteLine(@"Invalid sync operation: {0}", msioe.Message);
+			}
+			catch (Exception e)
+			{
+				Debug.WriteLine(@"Sync error: {0}", e.Message);
+			}
+			return null;
+		}
+
+		public async Task SaveTaskAsync(TodoItem item)
+		{
+			if (item.Id == null)
+			{
+				await todoTable.InsertAsync(item);
+			}
+			else
+			{
+				await todoTable.UpdateAsync(item);
+			}
+		}
+	}
+}
