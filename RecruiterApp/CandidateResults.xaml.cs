66b1ebea0b8efe50d778e81f783f7c103a32e8c9
mkenar <monica.kenar@us.sogeti.com>
2016-08-08 09:09:29 -0500

    Added changes to HistoryPage.

---
+++ RecruiterApp/CandidateResults.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RecruiterApp
+{
+	public partial class CandidateResults : ContentPage
+	{
+		public CandidateResults()
+		{
+			InitializeComponent();
+			//NavigationPage.SetBackButtonTitle(HistoryPage," ");
+			//NavigationPage.SetHasNavigationBar(HomePage,true);
+
+		}
+
+	}
+}
+
--- RecruiterApp/HistoryPage.xaml.cs
+++ RecruiterApp/HistoryPage.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
 
 namespace RecruiterApp
@@ -10,7 +9,31 @@ namespace RecruiterApp
 		public HistoryPage()
 		{
 			InitializeComponent();
+			HistoryListViewPage();
 		}
+
+		public void HistoryListViewPage()
+		{
+			var categories = new List<string>()
+			{
+				"View Accepted",
+				"View Rejected",
+				"Pipeline"
+			};
+
+			var list = historyListView;
+			list.ItemsSource = categories;
+			Content = list;
+		}
+
+		public void SelectionMade(object sender, ItemTappedEventArgs e)
+		{
+			var item = e.Item.ToString();
+			//DisplayAlert("Alert", "You have selected" + item, "OK");
+
+			Navigation.PushModalAsync(new CandidateResults());
+		}
+
 	}
 }
 
