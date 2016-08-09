fee9c1abfb3bb2e2fd9d81a9f32c1a44bc07ff5a
pintcris <cristian.pintado@us.sogeti.com>
2016-08-09 11:22:12 -0500

    Testing push for Monica

---
+++ RecruiterApp/ViewModels/PositionResultsPageModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecruiterApp
+{
+	public class PositionResultsPageModel
+	{
+		public Position positions { get; set; }
+		public List<Candidate> Candidates 
+		{ 
+			//Testing for monica
+			get 
+			{
+				return new List<Candidate>()
+				{
+					new Candidate {candidateId= 1, firstName="Monica", lastName="Kenar"},
+					new Candidate {candidateId=2, firstName="Eric", lastName="Ruelas"},
+					new Candidate {candidateId=3, firstName="Nikita", lastName="Belyaev"},
+					new Candidate {candidateId=4, firstName="Cristian", lastName="Pintado"}
+				};
+			}
+		}
+	}
+}
+
